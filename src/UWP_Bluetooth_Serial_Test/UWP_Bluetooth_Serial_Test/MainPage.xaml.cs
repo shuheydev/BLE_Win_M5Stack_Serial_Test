7f98fe441c29cc52c998957cba90867fab041ca4
shuhei.nishizawa <shuhei.nishizawa@avanade.com>
2022-06-24 19:33:50 +0900

    add uwp project

---
+++ src/UWP_Bluetooth_Serial_Test/UWP_Bluetooth_Serial_Test/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWP_Bluetooth_Serial_Test
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        SerialCommunication _serialCommunication;
+
+        DispatcherTimer _timer;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            _serialCommunication = new SerialCommunication();
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromMilliseconds(1000);
+            _timer.Tick += _timer_Tick;
+        }
+
+        private async void _timer_Tick(object sender, object e)
+        {
+            var receivedMessage = await _serialCommunication.Read();
+            textBox.Text = receivedMessage;
+        }
+
+        private async void ConnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            await _serialCommunication.Connect();
+            //_timer.Start();
+        }
+
+        private void StartReadingButton_Click(object sender, RoutedEventArgs e)
+        {
+            _timer.Start();
+        }
+    }
+}
---
+++ src/UWP_Bluetooth_Serial_Test/UWP_Bluetooth_Serial_Test/SerialCommunication.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.SerialCommunication;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml.Media.Animation;
+
+namespace UWP_Bluetooth_Serial_Test
+{
+    public class SerialCommunication
+    {
+        string _portName;
+        string _aqs;
+        SerialDevice _device;
+
+        DataReader _dataReader;
+        DataWriter _dataWriter;
+
+        public SerialCommunication()
+        {
+            _portName = "COM3";
+            _aqs = SerialDevice.GetDeviceSelector(_portName);
+        }
+
+        public async Task Connect()
+        {
+            //COMポート名を使ってデバイス候補を検索
+            var myDevices = await Windows.Devices.Enumeration.DeviceInformation.FindAllAsync(_aqs, null);
+
+            if (myDevices.Count == 0)
+            {
+                return;
+            }
+
+            _device = await SerialDevice.FromIdAsync(myDevices[0].Id);
+            _device.BaudRate = 115200;
+            _device.DataBits = 8;
+            _device.StopBits = SerialStopBitCount.One;
+            _device.Parity = SerialParity.None;
+            _device.Handshake = SerialHandshake.None;
+            _device.ReadTimeout = TimeSpan.FromMilliseconds(1000);
+            _device.WriteTimeout = TimeSpan.FromMilliseconds(1000);
+
+            _dataReader = new DataReader(_device.InputStream);
+            _dataWriter = new DataWriter(_device.OutputStream);
+        }
+
+        public async void Write()
+        {
+            _dataWriter.WriteString("Hello from M5Stack");
+            await _dataWriter.StoreAsync();
+        }
+
+
+        public async Task<string> Read()
+        {
+            await _dataReader.LoadAsync(128);
+            uint bytesToRead = _dataReader.UnconsumedBufferLength;
+            string receivedStrings = _dataReader.ReadString(bytesToRead);
+            if (string.IsNullOrEmpty(receivedStrings))
+            {
+                return string.Empty;
+            }
+
+            return receivedStrings;
+        }
+    }
+}
